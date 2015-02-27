4c04ffd1bb9081f247483fbadc4097abd6980995
Sydroid <857966575@qq.com>
2015-02-27 15:26:28 +0800

    Add the test ant use project to sshi.git

---
+++ src/main/AntMain.java
@@ -0,0 +1,14 @@
+package main;
+
+public class AntMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How to use ant to auto build java files.");
+
+	}
+
+}
