970f41cb8be03660113dcdf98148885bc573c1aa
sydroid <857966575@qq.com>
2015-05-05 14:44:52 +0800

    Code change from branch test to branch master.

--- src/main/AntMain.java
+++ src/main/AntMain.java
@@ -8,6 +8,9 @@ public class AntMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("How to use ant to auto build java files.");
+		System.out.println("master");
+		
+		System.out.println("test");
 
 	}
 
